edd7badceb83cca945f3ec22b73f3acc28c2b404
Igors <igors.gudelis@gmail.com>
2021-02-20 16:04:56 +0300

    feat: add header

--- src/app/modules/layout/components/index.ts
+++ src/app/modules/layout/components/index.ts
@@ -1,4 +1,5 @@
+import { HeaderComponent } from './header/header.component';
 import { PageContentComponent } from './page-content/page-content.component';
 
 export { PageContentComponent };
-export const LAYOUT_COMPONENTS = [PageContentComponent];
+export const LAYOUT_COMPONENTS = [HeaderComponent, PageContentComponent];
--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { LAYOUT_COMPONENTS } from './components';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [...LAYOUT_COMPONENTS],
+  declarations: [...LAYOUT_COMPONENTS, HeaderComponent],
   imports: [CommonModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
